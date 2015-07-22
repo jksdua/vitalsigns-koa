5da4723276b413337e07a2093116d7167ab0006a
Jaap Karan Singh <jksdua@gmail.com>
2015-07-22 10:18:07 +1000

    minor code style changes

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const DEFAULT_HTTP_UNHEALTHY = 503;
  * An endpoint suitable to attach to a Koa app.
  */
 module.exports = function(vitalsigns) {
-	return function *koaEndpoint() {
-		var report = vitalsigns.getReport(),
-			status = report.healthy ?
-				(vitalsigns._opts.httpHealthy || DEFAULT_HTTP_HEALTHY) :
-				(vitalsigns._opts.httpUnhealthy || DEFAULT_HTTP_UNHEALTHY);
+  return function *koaEndpoint() {
+    var report = vitalsigns.getReport(),
+      status = report.healthy ?
+        (vitalsigns._opts.httpHealthy || DEFAULT_HTTP_HEALTHY) :
+        (vitalsigns._opts.httpUnhealthy || DEFAULT_HTTP_UNHEALTHY);
 
-		this.status = status;
-		this.body = report;
-	};
+    this.status = status;
+    this.body = report;
+  }; // jshint ignore:line
 };
